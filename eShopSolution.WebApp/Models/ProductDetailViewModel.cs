855ad6f257a4852433d7e25f0fb4a600a6ac5482
LongDang136i <danglong136i@gmail.com>
2022-06-07 16:00:24 +0700

    update and fix bug, login

---
+++ eShopSolution.WebApp/Models/ProductDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using eShopSolution.ViewModel.Catalog.Categories;
+using eShopSolution.ViewModel.Catalog.Products;
+
+namespace eShopSolution.WebApp.Models
+{
+    public class ProductDetailViewModel
+    {
+        public CategoryVm Category { get; set; }
+
+        public ProductVm Product { get; set; }
+    }
+}
